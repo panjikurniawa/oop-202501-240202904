a713b55f9f785b1955fbad344f3ee2707925aa99
panjikurniawa <108813946+panjikurniawa@users.noreply.github.com>
2025-10-08 11:03:57 +0700

    Add files via upload

---
+++ praktikum/week1-setup-hello-pos/src/main/java/com/upb/agripos/HelloOOP.java
@@ -0,0 +1,31 @@
+// HelloOOP.java
+package com.upb.agripos;
+class Produk {
+    String nama;
+    int harga;
+    Produk(String nama, int harga) {
+        this.nama = nama;
+        this.harga = harga;
+    }
+}
+
+public class HelloOOP {
+    public static void main(String[] args) {
+        String nim = "240202904";
+        String namaMhs = "Panji Kurniawan";
+        Produk[] daftar = {
+                new Produk("Beras", 10000),
+                new Produk("Pupuk", 15000),
+                new Produk("Benih", 12000)
+        };
+        int total = 0;
+        System.out.println("Hello POS World");
+        System.out.println("NIM: " + nim + ", Nama: " + namaMhs);
+        System.out.println("Daftar Produk:");
+        for (Produk p : daftar) {
+            System.out.println("- " + p.nama + ": " + p.harga);
+            total += p.harga;
+        }
+        System.out.println("Total harga semua produk: " + total);
+    }
+}
