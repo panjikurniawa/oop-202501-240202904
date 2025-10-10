ae4fc1d363bf3d37f267f1395c212177992b57d7
panjikurniawa <108813946+panjikurniawa@users.noreply.github.com>
2025-10-10 13:20:15 +0700

    Add files via upload

---
+++ praktikum/week2-class-object/src/main/java/com/upb/agripos/MainProduk.java
@@ -0,0 +1,19 @@
+package com.upb.agripos;
+
+import com.upb.agripos.model.produk;
+import com.upb.agripos.util.CreditBy;
+
+public class MainProduk {
+    public static void main(String[] args) {
+        produk p1 = new produk("BNH-001", "Benih Padi IR64", 25000, 100);
+        produk p2 = new produk("PPK-101", "Pupuk Urea 50kg", 350000, 40);
+        produk p3 = new produk("ALT-501", "Cangkul Baja", 90000, 15);
+
+        System.out.println("Kode: " + p1.getKode() + ", Nama: " + p1.getNama() + ", Harga: " + p1.getHarga() + ", Stok: " + p1.getStok());
+        System.out.println("Kode: " + p2.getKode() + ", Nama: " + p2.getNama() + ", Harga: " + p2.getHarga() + ", Stok: " + p2.getStok());
+        System.out.println("Kode: " + p3.getKode() + ", Nama: " + p3.getNama() + ", Harga: " + p3.getHarga() + ", Stok: " + p3.getStok());
+
+        // Tampilkan identitas mahasiswa
+        CreditBy.print("240202904", "Panji Kurniawan");
+    }
+}
