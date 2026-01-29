cd177a22b2ba2295e532519b46a0fc48c816c58b
panjikurniawa <108813946+panjikurniawa@users.noreply.github.com>
2026-01-29 21:49:59 +0700

    Add files via upload

---
+++ praktikum/week13-gui-lanjutan/src/main/java/com/upb/agripos/AppJavaFX.java
@@ -0,0 +1,11 @@
+// AppJavaFX.java - PERBAIKI jadi seperti ini
+package com.upb.agripos;
+
+import com.upb.agripos.view.ProductTableView;
+
+public class AppJavaFX {
+    public static void main(String[] args) {
+        // Hanya panggil main dari ProductTableView
+        ProductTableView.main(args);
+    }
+}
