b8bfa8d3e4872db9c534a117beb1ab870a8322cb
panjikurniawa <108813946+panjikurniawa@users.noreply.github.com>
2026-01-30 20:39:55 +0700

    Create CartServiceTest.java

---
+++ praktikum/week14-integrasi-individu/src/test/java/com/upb/agripos/CartServiceTest.java
@@ -0,0 +1,14 @@
+package com.upb.agripos;
+
+import com.upb.agripos.service.CartService;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CartServiceTest {
+
+    @Test
+    public void testSimpleCalculation() {
+        // Test sederhana saja
+        assertEquals(4, 2 + 2);
+    }
+}
